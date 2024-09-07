17ade109c7735ff3036f0c2146f4bb66d196d906
MosesHe <h394421599@gmail.com>
2024-09-07 12:27:01 +0800

    Add settings page

--- luasrc/controller/seu_net_login.lua
+++ luasrc/controller/seu_net_login.lua
@@ -1,8 +1,9 @@
 module("luci.controller.seu_net_login", package.seeall)
 
 function index()
-    entry({"admin", "services", "seu_net_login"}, cbi("seu_net_login"), _("SEU Net Login"), 60)
-    entry({"admin", "services", "seu_net_login", "log"}, call("action_log"), _("Login Log"), 61)
+    entry({"admin", "services", "seu_net_login"}, firstchild(), _("SEU Net Login"), 60).dependent = false
+    entry({"admin", "services", "seu_net_login", "settings"}, cbi("seu_net_login"), _("Settings"), 1)
+    entry({"admin", "services", "seu_net_login", "log"}, call("action_log"), _("Log"), 2)
 end
 
 function action_log()
--- root/usr/bin/seu_net_login.sh
+++ root/usr/bin/seu_net_login.sh
@@ -19,5 +19,3 @@ response=$(curl -s "$AUTH_URL")
 echo "$(date): Request URL: $AUTH_URL" >> /tmp/seu_net_login.log
 echo "$(date): Response: $response" >> /tmp/seu_net_login.log
 echo "----------------------------------------" >> /tmp/seu_net_login.log
-
-echo "$response"
